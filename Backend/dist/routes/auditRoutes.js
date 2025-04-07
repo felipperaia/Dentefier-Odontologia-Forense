43996ca0667e1c40f74e3d5b80c23d0ac31d3c72
Felippe <83991212+felipperaia@users.noreply.github.com>
2025-04-07 15:49:16 -0300

    Delete Backend/dist directory
    remover a dist

--- Backend/dist/routes/auditRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auditController_1 = require("../controllers/auditController");
-const authMiddleware_1 = require("../middlewares/authMiddleware");
-const router = (0, express_1.Router)();
-// Obter logs de auditoria (somente administradores)
-router.get('/', authMiddleware_1.authenticateJWT, (0, authMiddleware_1.authorizeRoles)('admin'), auditController_1.getAuditLogs);
-exports.default = router;
